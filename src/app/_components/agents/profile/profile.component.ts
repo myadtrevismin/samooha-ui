fefceab99d09aeee4b5e6a1ba60654f8bb332232
Praveen Suram <praveenkumars219@gmail.com>
2020-10-02 22:43:05 -0400

    updates to profile screen

--- src/app/_components/agents/profile/profile.component.ts
+++ src/app/_components/agents/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { AccountService } from 'src/app/_services/account.service';
@@ -8,21 +10,29 @@ import { AccountService } from 'src/app/_services/account.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
 
   constructor(private route: ActivatedRoute,
               private accountService: AccountService) { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   profileId;
   agent: any;
   error;
   dataSource: MatTableDataSource<any>;
-  displayedColumns: string[] = ['sno', 'projectid', 'logo', 'name', 'date'];
+  displayedColumns: string[] = ['sno', 'projectid', 'logo', 'name', 'plots'];
 
   ngOnInit(): void {
     this.profileId = this.route.snapshot.paramMap.get('id');
     this.dataSource = new MatTableDataSource();
     this.loadAgentById();
+  }
+
+  // tslint:disable-next-line: typedef
+  ngAfterViewInit(){
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 
@@ -34,9 +44,10 @@ export class ProfileComponent implements OnInit {
                           if (x.subProjects !== null){
                             const projects = [];
                             x.subProjects.forEach(element => {
-                              projects.push(x.plot);
+                              projects.push(element);
                             });
                             this.dataSource.data = projects;
+                            console.log(projects);
                           }
                         }, (error) => this.error = error);
   }
