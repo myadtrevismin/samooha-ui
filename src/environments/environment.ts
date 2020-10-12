eff642e8e1563cb5574842834d44f46941f68433
Praveen Suram <praveenkumars219@gmail.com>
2020-10-12 15:33:16 -0400

    updated environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44385',
+  apiUrl: 'https://treviaplot.azurewebsites.net',
   bloburl: `vsadhanblob`,
   blobkey: `?sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-12-31T02:42:58Z&st=2020-09-19T17:42:58Z&spr=https,http&sig=%2FIxBLAxsf%2BtD684LdcEqUydAQPWzu3%2FbJX%2BmOV6koYQ%3D`
 };
